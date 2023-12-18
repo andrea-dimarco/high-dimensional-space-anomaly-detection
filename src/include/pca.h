85571d81f828b1d3ad914e3ae9ac81a9f5c46f2b
jemuelf98 <jemuelespiritufuentes@yahoo.it>
2023-12-18 15:35:43 +0100

    added eigen lib and pca skeleton

---
+++ src/include/pca.h
@@ -0,0 +1,60 @@
+// Geometric Entropy Minimization
+
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <cstring>
+#include <cstdlib>
+#include <assert.h>
+#include <cmath>
+#include <Eigen/Dense>
+#include <Eigen/src/Core/Map.h>
+#include <fstream>
+
+class PCA {
+/**
+ * Divide Data in subsets S1 and S2 with sizes N1 and N2
+ * Compute sample mean x_mean and covariance matrix Q on S1
+ * Compute eigenvalues and eigenvectors on Q
+ * Compute gamma 
+ * Compute r based on gamma and form matrix V
+*/
+private:
+    Eigen::MatrixXd S1;
+    Eigen::MatrixXd S2;
+    Eigen::VectorXd baseline_distances;
+    int N, N1, N2;   // set partition dimensions
+    float partition; // N1 should be 15% of N
+    double alpha;    // outlier detection hyperparameter
+    int p;           // dimensionality of the space
+    int k;           // neighbors to consider
+    double h;        // anomaly detection threshold
+    
+public:
+    PCA(int p, int k=4, double alpha=2.0, double h=5.0);
+
+    double euclidean_dist(Eigen::VectorXd p1, Eigen::VectorXd p2);
+
+    void partition_data(Eigen::MatrixXd X, float partition=0.15);
+
+    Eigen::MatrixXd getS1();
+    Eigen::MatrixXd getS2();
+    Eigen::MatrixXd getBaselineDistances();
+
+    Eigen::MatrixXd random_permutation(Eigen::MatrixXd X, bool columns=true, bool index_check=true);
+
+    void kNN(bool strict_k=false);
+
+    void save_baseline(std::string file_path="./baseline_distances.csv");
+    void load_baseline(std::string file_path="./baseline_distances.csv");
+
+    double CUSUM();
+
+    double tail_probability();
+
+    void offline_phase(Eigen::MatrixXd X, float partition=0.15, bool strict_k=false, bool save_file=true, std::string file_path="./baseline_distances.csv");
+
+    // return true if anomaly found
+    bool online_detection();
+};
---
+++ src/pca.cpp
@@ -0,0 +1,238 @@
+#include "include/pca.h"
+
+/**
+ * Constructor
+*/
+PCA::PCA(int p, int k/*=4*/,double alpha/*=2.0*/, double h/*=5.0*/) {
+    
+    assert((k>0) && (alpha>0) && (h>0));
+    assert(p>0);
+    this->k = k;
+    this->alpha = alpha;
+    this->h = h;
+    this->p = p;
+    // Offline Phase 
+    // Online Phase
+} /* PCA */
+
+/** 
+ * Given two vectors, returns the euclidean distance between them.
+*/
+double PCA::euclidean_dist(Eigen::VectorXd p1, Eigen::VectorXd p2) {
+    assert(p1.size() == p2.size());
+    return sqrt((p1-p2).dot((p1-p2)));
+} /* euclidean_dist */
+
+/**
+ * Given the set of nominal datapoints, randomly partitions the data in the two sets S1 and S2.
+ * TODO: find better way of randomly selecting samples, like generating M uniformly random indexes to define one of the two partitions?
+*/
+void PCA::partition_data(Eigen::MatrixXd X, float partition/*=0.15*/) {
+    
+    assert(X.rows() == this->p); // feature dimension must be on the y axis
+    assert((partition>0.0) && (partition<1.0));
+    
+    this->partition = partition;
+    this->N  = X.cols();
+    this->N1 = (int)(this->N*this->partition);
+    this->N1 = (this->N1 <= 0) ? 1 : this->N1;
+    this->N2 = this->N - this->N1;
+
+    this->S1.resize(this->p,this->N1);
+    this->S2.resize(this->p,this->N2);
+    this->baseline_distances.resize(this->N2);
+    this->baseline_distances.setZero();
+
+    // Don't forget the random shuffle!!
+    Eigen::MatrixXd X_perm = PCA::random_permutation(X);
+
+    int i, j, l;
+    i = 0;
+    for (j = 0; i < N1; j++) {
+        S1.col(j) = X_perm.col(i);
+        i++;
+    }
+    for (l = 0; l < N2; l++) {
+        S2.col(l) = X_perm.col(i);
+        i++;
+    }
+    assert(i == N); // we got every element
+} /* partition_data */
+
+/**
+ * Randomly permutates either the columns or the rows of the given matrix.
+ * Depending on the assignment of the boolean variable columns:
+ *  columns = true  -> permutates columns
+ *  columns = false -> permutate rows
+ * Also:
+ *  index_check = true  -> will NEVER try to swap the column with itself
+ *  index_check = false -> might swap the column with itseld (so no swap)
+*/
+Eigen::MatrixXd PCA::random_permutation(Eigen::MatrixXd X, bool columns/*=true*/, bool index_check/*=true*/) {
+    // Sanity check
+    if (columns) {
+        if (X.cols() == 1) {
+            std::cout << "Are you kidding me??" << std::endl;
+            return X;
+        }
+    } else {
+        if (X.rows() == 1) {
+            std::cout << "Are you kidding me??" << std::endl;
+            return X;
+        }
+    }
+
+    // Define a PRG
+    std::default_random_engine engine(0);
+    std::bernoulli_distribution bernoulli;
+    Eigen::MatrixXd X_perm = X;
+    int i, swap_index;
+    Eigen::MatrixXd tmp;
+
+    // Permutate
+    if (columns) {
+        std::uniform_int_distribution<> uniform_cols(0, X.cols()-1);
+        // randomly permutate columns
+        for (i = 0; i < X.cols(); i++) {
+            if (bernoulli(engine)) {
+                // swap column with another random column
+                do {
+                    swap_index = uniform_cols(engine);
+                } while((swap_index == i) && index_check); // don't swap the vector with itself!!
+                tmp = X_perm.col(i);
+                X_perm.col(i) = X_perm.col(swap_index);
+                X_perm.col(swap_index) = tmp;
+            }
+        }
+    } else {
+        std::uniform_int_distribution<> uniform_rows(0, X.rows()-1);
+        // randomly permutate rows
+        for (i = 0; i < X.rows(); i++) {
+            if (bernoulli(engine)) {
+                // swap column with another random column
+                do {
+                    swap_index = uniform_rows(engine);
+                } while((swap_index == i) && index_check); // don't swap the vector with itself!!
+                tmp = X_perm.row(i);
+                X_perm.row(i) = X_perm.row(swap_index);
+                X_perm.row(swap_index) = tmp;
+            }
+        }
+    }
+    return X_perm;
+} /* random_permutation */
+
+/** 
+ * Returns S1
+*/
+Eigen::MatrixXd PCA::getS1() {
+    return this->S1;
+}
+/** 
+ * Returns S2
+*/
+Eigen::MatrixXd PCA::getS2() {
+    return this->S2;
+}
+/** 
+ * Returns the baseline distances.
+ * Don't forget to compute them in the OFFLINE phase!!
+*/
+Eigen::MatrixXd PCA::getBaselineDistances() {
+    return this->baseline_distances;
+}
+
+/**
+ * Computes the k Nearest Neighbors of the set S2 in the set S1.
+ * if strict_k = true  it will return an error if the set S1 doesn't have enough neighbors
+ * else                it will compute the baseline with less than k neighbors if S1 is small
+*/
+void PCA::kNN(bool strict_k/*=false*/) {
+    // if these fail, you need to call PCA::partition_data(X) before kNN()!!
+    assert((this->N == (this->N1 + this->N2)));
+    assert(this->baseline_distances.size() == this->N2);
+    assert(this->S1.cols() == this->N1);
+    assert(this->S2.cols() == this->N2);
+    // not enough elements in the set to compute the k neighbors
+    assert((this->k <= this->N1) || !(strict_k));
+
+    Eigen::MatrixXd S1_sample, S2_sample;
+    Eigen::VectorXd tmp_distances(this->N1);
+    double dist, k_sum;
+    int i, j;
+    for (i = 0; i < this->N2; i++) {
+        // compute distances
+        S2_sample = this->S2.col(i);
+        tmp_distances.setZero();
+        for (j = 0; j < this->N1; j++) {
+            S1_sample = this->S1.col(j);
+            dist = PCA::euclidean_dist(S1_sample,S2_sample);
+            tmp_distances(j) = dist;
+        }
+        // sum the best k neighbors
+        std::sort(tmp_distances.begin(), tmp_distances.end());
+        k_sum = 0;
+        for (j = 0; (j < this->k) && (j < this->N1); j++) { k_sum += tmp_distances(j); }
+        // store value
+        this->baseline_distances(i) = k_sum;
+    }
+} /* kNN */
+
+/**
+ * Save the computed baseline in a .csv file
+*/
+void PCA::save_baseline(std::string file_path/*="./baseline_distances.csv"*/) {
+    
+    const Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
+
+	std::ofstream save_file(file_path);
+	if (save_file.is_open()) {
+		save_file << this->baseline_distances.format(CSVFormat);
+		save_file.close();
+	}
+} /* save_baseline */
+/**
+ * Load the previously computed baselines from a csv file
+*/
+void PCA::load_baseline(std::string file_path/*="./baseline_distances.csv"*/) {
+    std::vector<double> values;
+	
+	std::ifstream load_file(file_path); // store the data from the matrix
+	std::string row_string; // store the row of the matrix that contains commas 
+	std::string value; // store the matrix entry
+
+	while (std::getline(load_file, row_string)) // here we read a row by row of matrixDataFile and store every line into the string variable matrixRowString
+	{
+		std::stringstream row_stream(row_string); //convert matrixRowString that is a string to a stream variable.
+
+		while (std::getline(row_stream, value, ',')) // here we read pieces of the stream row_stream until every comma, and store the resulting character into the matrixEntry
+		{
+			values.push_back(std::stod(value));   //here we convert the string to double and fill in the row vector storing all the matrix entries
+		}
+	}
+	// here we conver the vector variable into the matrix and return the resulting object, 
+	// note that values.data() is the pointer to the first memory location at which the entries of the vector matrixEntries are stored;
+	this->baseline_distances = Eigen::Map<Eigen::Vector<double, Eigen::Dynamic>> (values.data(), values.size());
+} /* load_baseline */
+
+/**
+ * The whole offline phase of the PCA model.
+*/
+void PCA::offline_phase(Eigen::MatrixXd X, float partition/*=0.15*/,
+                    bool strict_k/*=false*/, bool save_file/*=true*/, 
+                    std::string file_path/*="./baseline_distances.csv"*/) {
+    // sanity check
+    assert(X.rows() == this->p);
+
+    PCA::partition_data(X, partition);
+    PCA::kNN(strict_k);
+    if (save_file) { PCA::save_baseline(file_path); }
+}
+
+bool PCA::online_detection() {
+
+    bool anomaly_found = false;
+
+
+    return anomaly_found;
+}
