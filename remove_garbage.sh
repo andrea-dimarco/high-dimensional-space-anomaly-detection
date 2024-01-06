5ee5855733568f19900f26148dd515de6fbd4afc
Andrea Di Marco <andr.dima99@gmail.com>
2024-01-06 17:41:22 +0100

    updated garbage collection

--- remove_garbage.sh
+++ remove_garbage.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm ./res_proj.csv
-rm ./parameters.csv
-rm ./mean_vector.csv
-rm ./baseline_distances.csv
-rm ./S1.csv
+rm ./pca_res_proj.csv
+rm ./pca_parameters.csv
+rm ./pca_mean_vector.csv
+rm ./pca_baseline_distances.csv
+
+rm ./gem_parameters.csv
+rm ./gem_baseline_distances.csv
+rm ./gem_S1.csv
